6eb91f5010fc890995ae659507a5fdb56042fc73
Paul Gundarapu <leogps@gmail.com>
2025-03-30 11:16:27 -0700

    workflow updates.

--- setup.py
+++ setup.py
@@ -56,19 +56,5 @@ setup(
               'server',
               'http-server'],
     install_requires=REQUIREMENTS,
-    python_requires='>=3.4',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
+    python_requires='>=3.4'
 )
